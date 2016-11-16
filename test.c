7bc53baf40b9983070982984a37a80c497c0b778
Minseong Jeong <msjeong482@gmail.com>
2016-11-16 21:48:29 +0900

    ver 1.2

--- test.c
+++ test.c
@@ -5,6 +5,8 @@ int main (void) {
 
 	printf("this line is add test in internet\n");
 	printf("this line is add test in internet\n");
+	printf("this line is add test by linux\n");
+	printf("this line is add test by linux\n");
 	
 	return 0;
 }
