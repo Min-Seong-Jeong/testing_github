14f85483a5e884faf06266a738d8d7fda89b0e6e
Minseong Jeong <msjeong482@gmail.com>
2016-11-16 21:47:41 +0900

    Update test.c

--- test.c
+++ test.c
@@ -5,6 +5,8 @@ int main (void) {
 
 	printf("this line is add test in internet\n");
 	printf("this line is add test in internet\n");
+	printf("this line is add test in internet\n");
+	printf("this line is add test in internet\n");
 	
 	return 0;
 }
