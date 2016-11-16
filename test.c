666e3d28143d51416dffaa0f87ff04381916a7c4
Minseong Jeong <msjeong482@gmail.com>
2016-11-16 21:59:12 +0900

    pull 2.0

--- test.c
+++ test.c
@@ -7,6 +7,8 @@ int main (void) {
 	printf("this line is add test in internet\n");
 	printf("this line is add test by linux\n");
 	printf("this line is add test by linux\n");
+	printf("this line is add test in internet\n");
+	printf("this line is add test in internet\n");
 	
 	return 0;
 }
