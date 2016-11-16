361ff3e67e12a61d1f22e8f5311d0a6bbd8e4007
Minseong Jeong <msjeong482@gmail.com>
2016-11-16 22:50:03 +0900

    test 4

--- test.c
+++ test.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
-
+what the
 int main (void) {
 
 	printf("this line is add test in internet\n");
+	step 1
 	this is linux zz	
+	step 2
 	printf("this line is add test in internet\n");
+	step 3
 	printf("this line is add test in internet\n");
-	add 1
+	step 4
+	step 5
 	add 2
+	step 6
 }
+
+zzz
