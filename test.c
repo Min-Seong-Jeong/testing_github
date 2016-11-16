29361f4dd1d60cbc2eb9009e26b71a47bb6b0561
Minseong Jeong <msjeong482@gmail.com>
2016-11-16 22:40:22 +0900

    test 2

--- test.c
+++ test.c
@@ -4,12 +4,7 @@ int main (void) {
 	int a;
 
 	printf("this line is add test in internet\n");
-	printf("this line is add test in internet\n");
-	printf("this line is add test by linux\n");
-	printf("this line is add test by linux\n");
-	printf("this line is add test in internet\n");
-	
+	this is linux zz	
 	printf("this line is add test in internet\n");
 	printf("this line is add test in internet\n");
-	return 0;
 }
